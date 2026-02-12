f504a8c67888ffd3a706039070e0afdc442b7ced
Espoir Mulela <espoirmulex@gmail.com>
2026-02-12 16:57:03 +0100

    feat: add memes table migration

---
+++ database/migrations/2026_02_12_155601_create_memes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('memes', function (Blueprint $table) {
+            $table->id();
+            $table->string('image_path');
+            $table->string('top_text', 100)->nullable();
+            $table->string('bottom_text', 100)->nullable();
+            $table->timestamps();
+
+            $table->index('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('memes');
+    }
+};
